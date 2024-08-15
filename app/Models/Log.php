26567d1f06e72f027e5d65fe14d3ac4165fb7815
Doni Fitriansyah <102551960+donifitriansyah@users.noreply.github.com>
2024-08-15 11:03:51 +0700

    Laporan Controller

--- app/Models/Log.php
+++ app/Models/Log.php
@@ -8,6 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Log extends Model
 {
     use HasFactory;
-    
+    public function presensis()
+{
+    return $this->hasMany(Presensi::class, 'id_tahun_ajar');
+}
     protected $guarded = [];
 }
